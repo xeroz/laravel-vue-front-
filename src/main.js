760cfcca47ae2df1b651ccc29cf8020efb8b0a3b
juan <chavezvasquezjuan@gmail.com>
2017-02-24 11:36:16 -0500

    main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from './routes.js'
+
+import VueResource from 'vue-resource'
+// import Auth from './packages/auth/auth.js'
+
+Vue.use(VueResource)
+// Vue.use(Auth)
+
+// Router.beforeEach(
+//     (to, from, next) => {
+//         if(to.matched.some(record => record.metaforVisitors)){
+//             if(Vue.auth.isAuthenticated()){
+//                 next({
+//                     path: '/feed'
+//                 })
+//             } else next()
+//         } else next()
+//     }
+// )
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+   el: '#app',
+   render: h => h(App),
+   router: Router
 })
